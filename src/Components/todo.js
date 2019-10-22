b6c9fe9d8611ffd279741eeb8a5ab103dfc4b828
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-22 11:22:44 +0800

    Working add list

--- src/Components/todo.js
+++ src/Components/todo.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import './todo.css';
 
-class TodoList extends React.Component {
+class Todo extends React.Component {
+
     state = {
-        value : ''
+        value : '',
+        task: []
     };
+
     handleChange = (e) => {
-        this.setState({input: e.target.value})
-        console.log(this.setState({input: e.target.value}))
-    }
+        this.setState({value: e.target.value})
+    };
+
+    handleAdd = () => {
+        this.setState(state =>{
+            const taskList = this.state.task.push(<div><input type="checkbox"/><span>{this.state.value}</span></div>);
+            return {taskList, value: ''}
+        });
+    };
+    
 
     render() {
-        const { value } = this.state;
         return (
             <div className="todo-group">
                 <div className="add-button">
-                    <input type="text" onChange={this.handleChange}/>
-                    <button onClick={this.handleAdd}>Add Item</button>                   
-                </div> 
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    <button onClick={this.handleAdd}>Add Item</button>
+                    <ul>
+                        {this.state.task.map((item, index) => (
+                            <li key={index} id={item}>{item}</li>
+                        ))}
+                    </ul>             
+                </div>
             </div>
         )
     }
 }
 
-export default TodoList;
+export default Todo;
 
